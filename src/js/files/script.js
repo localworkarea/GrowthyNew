56d04e4b0f8845a85c0889774e6478d3f5a6ecb6
Denys Mykhailenko <localworkarea@gmail.com>
2024-01-23 15:25:41 +0200

    update

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -12,16 +12,3 @@ if (LineMove) {
   const copyLine2 = document.querySelector(".trust-brand__list-2").cloneNode(true);
   document.querySelector(".trust-brand__body_02").appendChild(copyLine2);
 }
-
-    document.addEventListener('DOMContentLoaded', function () {
-        var casesLinks = document.querySelectorAll('.cases__link');
-        casesLinks.forEach(function (link) {
-            link.addEventListener('click', function (event) {
-                casesLinks.forEach(function (otherLink) {
-                    otherLink.classList.remove('active');
-                });
-                // Add the 'active' class to the clicked link
-                link.classList.add('active');
-            });
-        });
-    });
